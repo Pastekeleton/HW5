9435333cd5dac88773552b6fc0df91de76ee9307
David <david.melo@lc.cuny.edu>
2023-04-19 19:50:00 -0400

    2nd

--- Server/index.js
+++ Server/index.js
@@ -6,4 +6,4 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`)})
