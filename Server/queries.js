fbf1b02fddac8efa5e57da8e2ddbffd50760ce2b
David <david.melo@lc.cuny.edu>
2023-04-26 19:10:45 -0400

    fixed queries

--- Server/queries.js
+++ Server/queries.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'links',
+  database: 'favlinks',
   password: '0Gazeller3d1',
   port: 5432,
 })
